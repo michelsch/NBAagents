970ef85a215eb2b984d0d44fb7e4e03dacbd6634
michaelsch92 <michel.schoemaker@gmail.com>
2015-11-21 17:05:12 -0800

    commit test

--- player.py
+++ player.py
@@ -2,6 +2,68 @@ import util
 import math
 import random
 
-class Player:
+'''NBA Teams:
+
+  Atlantic:
+    Boston Celtics
+    Brooklyn Nets
+    New York Knicks
+    Philadelphia 76ers
+    Toronto Raptors
+
+  Southwest:
+    Dallas Mavericks
+    Houston Rockets
+    Memphis Grizzlies
+    New Orleans Pelicans
+    San Antonio Spurs
+
+  Central:
+    Chicago Bulls
+    Cleveland Cavaliers
+    Detroit Pistons
+    Indiana Pacers
+    Milwaukee Bucks
+
+  Northwest:
+    Denver Nuggets
+    Minnesota Timberwolves
+    Oklahoma City Thunder
+    Portland Trail Blazers
+    Utah Jazz
+
+  Southeast:
+    Atlanta Hawks
+    Charlotte Hornets
+    Miami Heat
+    Orlando Magic
+    Washington Wizards
+
+  Pacific:
+    Golden State Warriors
+    Los Angeles Clippers
+    Los Angeles Lakers
+    Phoenix Suns
+    Sacramento Kings
+'''
+
+class Team:
+'''Defines a team composed of players and a coach'''
+  numTeams = 0
 
   def __init__(self):
+
+class Player(Team):
+'''Defines a player for an NBA team'''
+  numPlayers = 0
+
+  def __init__(self):
+
+class Coach(Team):
+'''Defines a coach for an NBA team'''
+
+  def __init__(self):
+
+def initializeTeams():
+
+
